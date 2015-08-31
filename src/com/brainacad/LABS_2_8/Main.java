23db2e825debaeb157710726c6166354a9f7fa73
VitaliyVorona <voronchik84@gmail.com>
2015-08-31 20:40:02 +0300

    LABS_2_8 and  Main.java class are added

---
+++ src/com/brainacad/LABS_2_8/Main.java
@@ -0,0 +1,10 @@
+package com.brainacad.LABS_2_8;
+
+/**
+ * Created by User on 31.08.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
