5821517a1df8dee8ffe7be0cd59672432af956f2
Vitaliy Vorona <vitaliy.vorona@avid.com>
2015-09-07 17:47:44 +0300

    parseShape from labs 9 is added

--- src/com/brainacad/LABS_2_7/Main.java
+++ src/com/brainacad/LABS_2_7/Main.java
@@ -10,29 +10,9 @@ import com.brainacad.LABS_2_7.labs_2_7_6.Triangle;
  */
 public class Main {
     public static void main(String[] args) {
-        Integer a = new Integer(2);
-
-        double area = 0;
-
-        int z = a.MAX_VALUE;
-//        Shape shape = new Shape("GREEN");
-        Cirlce cirlce1 = new Cirlce("RANGE", 5);
-        Cirlce cirlce2 = new Cirlce("ORANGE", 10);
-        Rectangle rectangle1 = new Rectangle("BLUE", 4, 5);
-        Rectangle rectangle2 = new Rectangle("YELLOW", 4, 5);
-//        Triangle triangle2 = new Triangle("BRED", 5, 5, 5);
-        Triangle triangle1 = new Triangle("RED", 25, 5, 5);
-//        Triangle triangle3 = new Triangle("FRED", 25, 5, 5);
-//        Triangle triangle4 = new Triangle("DRED", 5, 5, 35);
-//        Triangle triangle5 = new Triangle("SRED", 54, 35, 25);
-//        triangle2, triangle3, triangle4, triangle5,
-        Shape[] shapes = new Shape[]{triangle1, cirlce1, cirlce2, rectangle1, rectangle2};
-        System.out.println(rectangle1.compareTo(cirlce1));
-        for (Shape i : shapes) {
-//            System.out.println(i.);
-//            area = area + i.calcArea();
-            i.draw();
-        }
-        }
-
+        Shape cirlce = Shape.parseShape("Circle:BLACK:10");
+        Shape triangle = Shape.parseShape("Triangle:GREEN:9,7,12");
+        cirlce.toString();
     }
+
+}
--- src/com/brainacad/LABS_2_7/labs_2_7_6/Shape.java
+++ src/com/brainacad/LABS_2_7/labs_2_7_6/Shape.java
@@ -5,10 +5,10 @@ import com.brainacad.LABS_2_8.labs_2_8_1.Drawable;
 /**
  * Created by User on 31.08.2015.
  */
-public abstract class Shape implements Drawable, Comparable{
+public abstract class Shape implements Drawable, Comparable {
     private String shapeColor;
 
-    public void draw(){
+    public void draw() {
         System.out.println(this.toString());
     }
 
@@ -17,6 +17,49 @@ public abstract class Shape implements Drawable, Comparable{
     }
 
     public abstract double calcArea();
+
+
+    public static Shape parseShape(String obj) throws NullPointerException {
+        String shape = obj.substring(0, obj.indexOf(":"));
+        Shape shapeObj = null;
+        if (shape.equals("Rectangle")) {
+            String[] params = paramsRectangle(obj);
+            shapeObj = new Rectangle(params[0].toString(), Double.parseDouble(params[1]), Double.parseDouble(params[2]));
+        }
+        if (shape.equals("Triangle")) {
+            String[] params = paramsTriangle(obj);
+            shapeObj = new Triangle(params[0].toString(), Double.parseDouble(params[1]), Double.parseDouble(params[2]), Double.parseDouble(params[3]));
+        }
+        if (shape.equals("Circle")) {
+            String[] params = paramsCircle(obj);
+            shapeObj = new Cirlce(params[0].toString(), Double.parseDouble(params[1]));
+        }
+        return shapeObj;
+    }
+
+    public static String[] paramsRectangle(String params) {
+        String[] paramsList = new String[3];
+        paramsList[0] = params.substring(params.indexOf(":"), params.lastIndexOf(":"));
+        paramsList[1] = params.substring(params.lastIndexOf(":"), params.indexOf(","));
+        paramsList[2] = params.substring(params.indexOf(","), params.length());
+        return paramsList;
+    }
+
+    public static String[] paramsTriangle(String params) {
+        String[] paramsList = new String[3];
+        paramsList[0] = params.substring(params.indexOf(":"), params.lastIndexOf(":"));
+        paramsList[1] = params.substring(params.lastIndexOf(":"), params.indexOf(","));
+        paramsList[2] = params.substring(params.indexOf(","), params.lastIndexOf(","));
+        paramsList[3] = params.substring(params.lastIndexOf(","), params.length());
+        return paramsList;
+    }
+
+    public static String[] paramsCircle(String params) {
+        String[] paramsList = new String[3];
+        paramsList[0] = params.substring(params.indexOf(":"), params.lastIndexOf(":"));
+        paramsList[1] = params.substring(params.lastIndexOf(":"+1), params.length());
+        return paramsList;
+    }
 
     @Override
     public String toString() {
