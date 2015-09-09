895efeb57362bd14489c7a5208b6977254a2b8d8
VitaliyVorona <voronchik84@gmail.com>
2015-09-09 20:43:46 +0300

    labs 2_12_3 class Card is added

--- src/com/brainacad/LABS_2_12/Main.java
+++ src/com/brainacad/LABS_2_12/Main.java
@@ -5,6 +5,32 @@ package com.brainacad.LABS_2_12;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        System.out.println(MyDayOfWeek.SATURDAY.nextDay());
+    }
+
+    public enum MyDayOfWeek {
+        SUNDAY,
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY;
+
+        public MyDayOfWeek nextDay() {
+            int day = this.ordinal();
+            if (this == SATURDAY) {
+                return SUNDAY;
+            } else return MyDayOfWeek.values()[++day];
+        }
+    }
+
+    public enum Suit {
+        SPADE, DIAMOND, CLUB, HEART;
+    }
+
+    public enum Rank {
+        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
     }
 }
---
+++ src/com/brainacad/LABS_2_12/labs_2_12_3/Card.java
@@ -0,0 +1,7 @@
+package com.brainacad.LABS_2_12.labs_2_12_3;
+
+/**
+ * Created by User on 09.09.2015.
+ */
+public class Card {
+}
